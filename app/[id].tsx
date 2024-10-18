fc9a31ae9e4f461ecbbb46143e7e07f9fe75e5d6
yashas <yashasbharadwaj6@gmail.com>
2024-10-18 19:33:59 +0530

    event details page

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,6 +1,6 @@
-import { View, Text } from 'react-native';
-import { useLocalSearchParams } from 'expo-router';
-
+import { View, Text, Image } from 'react-native';
+import { useLocalSearchParams, Stack } from 'expo-router';
+import dayjs from 'dayjs';
 import events from '../assets/events.json';
 
 export default function EventPage() {
@@ -8,9 +8,25 @@ export default function EventPage() {
 
   const event = events.find((event) => event.id === id);
 
+  if (!event) {
+    return <Text>Event not found</Text>;
+  }
+
   return (
     <>
-      <Text>Event title : {event?.title}</Text>
+      <View className="flex-1 gap-3 bg-white p-3">
+        <Stack.Screen
+          options={{ title: 'Event', headerBackTitleVisible: false, headerTintColor: 'black' }}
+        />
+        <Image source={{ uri: event.image }} className="aspect-video w-full rounded-xl" />
+        <Text>{event.title}</Text>
+        <Text className="text-lg font-semibold uppercase text-amber-800">
+          {dayjs(event.datetime).format('ddd, MMM D')} -{dayjs(event.datetime).format('h:mm A')}
+        </Text>
+        <Text className="text-lg" numberOfLines={2}>
+          {event.description}
+        </Text>
+      </View>
     </>
   );
 }
